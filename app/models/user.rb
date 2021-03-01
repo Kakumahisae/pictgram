ec1d12c1603ae306966e68a8314defc8fd04d092
KakumaHisae <mabel8mapel@yahoo.co.jp>
2021-03-01 09:09:14 +0000

    3/1third

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   
   has_secure_password
-  
+  has_many :topics
   has_many :favorites
   has_many :favorite_topics, through: :favorites, source: 'topic'
   
